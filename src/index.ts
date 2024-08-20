8ddefe0865378421b80defede8afb96b2c37cb34
Removed Contributor <thecodermehedi@gmail.com>
2024-08-20 18:11:16 +0600

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import cors from 'cors';
+import morgan from 'morgan';
+import http, { Server } from 'http'
+import cookieParser from "cookie-parser";
+import express, { type Application } from 'express';
+
+
+export const app: Application = express()
+export const server: Server = http.createServer(app)
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'));
+app.use(cors({ origin: ['http://localhost:5173'] }));
+

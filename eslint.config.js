bd51fa9d6911b7d4e212e7d689e2ecf925dfec77
Removed Contributor <iammehedihasan@protonmail.com>
2024-08-31 00:06:28 +0600

    chore: Update eslint.config.js to include TypeScript linting rules and configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,34 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettierConfig from "eslint-config-prettier"
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import node from "eslint-plugin-node";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettierConfig,
+
+export default [
   {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tsParser,
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      node,
+      "@typescript-eslint": tsPlugin,
+    },
     rules: {
-      "no-undef": "error",
-      "no-unused-vars": "error",
-      "no-unused-expressions": "error",
-      "prefer-const": "error",
+      "no-unused-vars": "warn",
+      "semi": ["error", "always"],
       "no-console": "warn",
-      "@typescript-eslint/no-explicit-any": "warn"
     },
   },
-  {
-    ignores: ["**/node_modules/", "**/build/"],
-  },
-);
+  prettierConfig,
+];

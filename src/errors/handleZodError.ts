f7cc93889d24c82669e7efec45cd476f3f0cf084
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-06 23:17:57 +0600

    Add handleZodError function for handling Zod validation errors

---
+++ src/errors/handleZodError.ts
@@ -0,0 +1,18 @@
+import type { ZodError, ZodIssue } from "zod";
+import type { TErrorObject, TReturnError } from "./error.types";
+import httpStatus from "http-status";
+
+const handleZodError = (err: ZodError): TReturnError => {
+  return {
+    code: httpStatus.BAD_REQUEST,
+    message: err.name,
+    details: err.issues.map((issue: ZodIssue): TErrorObject => {
+      return {
+        path: issue.path.join(' --> '),
+        message: issue.message,
+      };
+    }),
+  };
+};
+
+export default handleZodError;

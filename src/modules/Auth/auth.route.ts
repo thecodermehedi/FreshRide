0037a891ef7a36354692ea0e25c014728cc1d6ef
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-06 23:12:46 +0600

    Refactor auth.route.ts to use middleware for request validation

--- src/modules/Auth/auth.route.ts
+++ src/modules/Auth/auth.route.ts
@@ -1,11 +1,20 @@
-import { createRouter, validateRequest } from '../../utils';
-import { AuthControllers } from './auth.controller';
-import { AuthValidations } from './auth.validator';
+import validateRequest from '../../middlewares/validateRequest';
+import {createRouter} from '../../utils';
+import {AuthControllers} from './auth.controller';
+import {AuthValidations} from './auth.validator';
 
 const authRouter = createRouter();
 
-authRouter.post('/login', validateRequest(AuthValidations.loginValidationSchema), AuthControllers.login);
+authRouter.post(
+  '/login',
+  validateRequest(AuthValidations.loginValidationSchema),
+  AuthControllers.login,
+);
 
-authRouter.post('/signup', validateRequest(AuthValidations.signupValidationSchema), AuthControllers.signup);
+authRouter.post(
+  '/signup',
+  validateRequest(AuthValidations.signupValidationSchema),
+  AuthControllers.signup,
+);
 
 export const authRoutes = authRouter;

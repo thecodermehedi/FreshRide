752a9311f1e8f09d7d2bb6279d0515584bb2444e
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-06 23:18:02 +0600

    Add auth middleware for handling user authentication and authorization

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import config from '../config';
+import AppError from '../errors/AppError';
+import UserModel from '../modules/User/user.model';
+import jwt, {type JwtPayload} from 'jsonwebtoken';
+import type {TUserRole} from '../modules/User/user.types';
+import {catchAsync} from '../utils';
+import httpStatus from 'http-status';
+
+const auth = (...requiredRoles: Array<TUserRole>) =>
+  catchAsync(async (req, res, next) => {
+    const token = req?.headers?.authorization;
+    if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+    }
+    const decoded = jwt.verify(token, config.jwtAccessSecret) as JwtPayload;
+    const user = await UserModel.findOne({email: decoded.email});
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+    if (requiredRoles.length && !requiredRoles.includes(decoded?.role)) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+    }
+    req.user = decoded;
+    next();
+  });
+
+export default auth;

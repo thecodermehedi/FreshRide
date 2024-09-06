61ccd495a20671fe45154dbe5a19c62c02c141ff
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-06 23:12:56 +0600

    Refactor auth.controller.ts to improve login and signup functionality

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -1,29 +1,36 @@
-import type { RequestHandler } from "express";
-import { catchAsync } from "../../utils";
-import { AuthServices } from "./auth.service";
-import config from "../../config";
+import type {RequestHandler} from 'express';
+import {catchAsync} from '../../utils';
+import {AuthServices} from './auth.service';
+import config from '../../config';
+import httpStatus from 'http-status';
 
 const login: RequestHandler = catchAsync(async (req, res) => {
   const result = await AuthServices.login(req.body);
-  res.cookie('refreshToken', result.refreshToken, {
-    secure: config.nodeEnv === 'production',
-    httpOnly: true,
-  }).json({
-    success: true,
-    statusCode: 200,
-    message: "User logged in successfully",
-    token: result.accessToken,
-    data: result.data
-  })
-
-})
+  res
+    .cookie('refreshToken', result.refreshToken, {
+      secure: config.nodeEnv === 'production',
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'User logged in successfully',
+      token: result.accessToken,
+      data: result.data,
+    });
+});
 
 const signup: RequestHandler = catchAsync(async (req, res) => {
-  const result = await AuthServices.signup(req.body)
-})
-
+  const result = await AuthServices.signup(req.body);
+  res.json({
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'User created successfully',
+    data: result,
+  });
+});
 
 export const AuthControllers = {
   login,
-  signup
-}
+  signup,
+};

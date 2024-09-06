74771d33dd28c2893b77cfdcc048a410ffc619ad
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-06 23:13:31 +0600

    Refactor auth.service.ts for improved login and signup functionality

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,44 +1,60 @@
 import bcrypt from 'bcrypt';
-import UserModel from "../User/user.model";
-import type { TLogin, TUser } from "./auth.types";
-import { createToken } from './auth.utils';
+import UserModel from '../User/user.model';
+import type {TLogin, TUser} from './auth.types';
+import {createToken} from './auth.utils';
 import config from '../../config';
+import httpStatus from 'http-status';
 
 const login = async (params: TLogin) => {
-
   // Check if user exists by querying the user with recieved email
-  const userDetails = await UserModel.findOne({ email: params.email }).select('+password')
+  const userDetails = await UserModel.findOne({email: params.email}).select(
+    '+password',
+  );
   const isUserExists = !!userDetails;
   if (!isUserExists) {
     //TODO: Handle Error from here
-    throw { status: 404, message: 'User not found' };
+    throw {status: httpStatus.NOT_FOUND, message: 'User not found'};
   }
 
   // Check if the provide password is correct or not
   let isPasswordMatched: boolean = false;
   if (isUserExists && userDetails.password) {
-    isPasswordMatched = await bcrypt.compare(params.password, userDetails.password);
+    isPasswordMatched = await bcrypt.compare(
+      params.password,
+      userDetails.password,
+    );
   }
   if (!isPasswordMatched) {
     //TODO: Handle Error from here
-    throw { status: 401, message: 'Invalid password' };
+    throw {status: httpStatus.UNAUTHORIZED, message: 'Invalid password'};
   }
 
   // Generate AccessToken with userdetails
-  const payload = { email: userDetails.email, role: userDetails.role };
-  const accessToken = createToken(payload, config.jwtAccessSecret, config.jwtAccessExpiresIn);
-  const refreshToken = createToken(payload, config.jwtRefreshSecret, config.jwtRefreshExpiresIn)
+  const payload = {email: userDetails.email, role: userDetails.role};
+  const accessToken = createToken(
+    payload,
+    config.jwtAccessSecret,
+    config.jwtAccessExpiresIn,
+  );
+  const refreshToken = createToken(
+    payload,
+    config.jwtRefreshSecret,
+    config.jwtRefreshExpiresIn,
+  );
 
   // Return Access Token, Refresh Token
-  return { accessToken, refreshToken, data: userDetails }
-}
+  return {accessToken, refreshToken, data: userDetails};
+};
 
 const signup = async (params: TUser) => {
-  
-}
-
+  const isUserExists = await UserModel.findOne({email: params.email});
+  if (isUserExists) {
+    throw {status: httpStatus.CONFLICT, message: 'User already exists'};
+  }
+  return await UserModel.create(params);
+};
 
 export const AuthServices = {
   login,
-  signup
-}
+  signup,
+};

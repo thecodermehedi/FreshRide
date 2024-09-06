4ad6045ea63f9ea8901509911acc3b9afce2c125
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-06 23:17:28 +0600

    Add handleValidationError function for handling mongoose validation errors

---
+++ src/errors/handleValidationError.ts
@@ -0,0 +1,20 @@
+import type mongoose from "mongoose";
+import type { TErrorObject, TReturnError } from "./error.types";
+import httpStatus from "http-status";
+
+type TMongooseError = mongoose.Error.ValidatorError | mongoose.Error.CastError;
+
+const handleValidationError = (err: mongoose.Error.ValidationError): TReturnError => {
+  return {
+    code: httpStatus.BAD_REQUEST,
+    message: 'Mongoose Error',
+    details: Object.values(err.errors).map((issue: TMongooseError): TErrorObject => {
+      return {
+        path: issue?.path,
+        message: issue?.message,
+      };
+    }),
+  };
+};
+
+export default handleValidationError;

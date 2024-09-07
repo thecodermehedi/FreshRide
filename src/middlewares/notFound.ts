a271cc132d84e2cfec8649580581fa5787f0b359
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-07 07:32:43 +0600

    Refactor notFound middleware to improve error handling

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -2,20 +2,21 @@ import config from '../config';
 import httpStatus from 'http-status';
 import type {RequestHandler} from 'express';
 import {getCurrentDateTime} from '../utils';
-const notFound: RequestHandler = (req, res) => {
-  return res.status(httpStatus.NOT_FOUND).json({
-    status: 'error',
-    message: 'Resource not found',
-    timestamp: getCurrentDateTime(),
-    ...(config.nodeEnv !== 'production'
-      ? {
-          debugInfo: {
-            method: req.method ?? 'no method provided',
-            url: req.url ?? 'no url provided',
-          },
-        }
-      : {}),
-  });
+
+const notFound: RequestHandler = (req, res, next) => {
+  const error = new Error('Not Found');
+  res.status(httpStatus.NOT_FOUND);
+  res.locals.error = error;
+  res.locals.timestamp = getCurrentDateTime();
+
+  if (config.nodeEnv !== 'production') {
+    res.locals.debugInfo = {
+      method: req.method ?? 'no method provided',
+      url: req.url ?? 'no url provided',
+    };
+  }
+
+  next(error);
 };
 
 export default notFound;

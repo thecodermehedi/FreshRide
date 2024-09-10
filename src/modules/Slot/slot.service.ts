e9797c7e1034333f6cddd553b29da36db19f6979
Removed Contributor <thecodermehedi@gmail.com>
2024-09-10 18:24:24 +0600

    added get slots service and fixed createSlots service

--- src/modules/Slot/slot.service.ts
+++ src/modules/Slot/slot.service.ts
@@ -1,9 +1,9 @@
 import httpStatus from 'http-status';
 import SlotModel from './slot.model';
-import type {TSlot} from './slot.types';
+import type { TSlot } from './slot.types';
 import AppError from '../../errors/AppError';
 import ServiceModel from '../Service/service.model';
-import {addMinutes, differenceInMinutes, format, parse} from 'date-fns';
+import { addMinutes, differenceInMinutes, format, parse } from 'date-fns';
 
 const createSlots = async (slotData: TSlot) => {
   const serviceDetails = await ServiceModel.findById(slotData.service);
@@ -16,7 +16,6 @@ const createSlots = async (slotData: TSlot) => {
   const serviceDurationInMinutes = serviceDetails.duration;
   const slotStartTime = slotData.startTime;
   const slotEndTime = slotData.endTime;
-  const slotDate = slotData.date;
 
   // Parse time strings to Date objects
   const slotStartDate = parse(slotStartTime, 'HH:mm', new Date());
@@ -31,20 +30,36 @@ const createSlots = async (slotData: TSlot) => {
   // Generate slots with startTime and endTime starting from slotStartTime
   const slots = [];
   for (let i = 0; i < numberOfSlots; i++) {
-    const start = addMinutes(slotStartDate, i * serviceDurationInMinutes);
+    const start = addMinutes(slotStartTime, i * serviceDurationInMinutes);
     const end = addMinutes(start, serviceDurationInMinutes);
     const slot = {
-      service: serviceDetails._id,
-      date: slotDate,
+      service: slotData.service,
+      date: slotData.date,
       startTime: format(start, 'HH:mm'),
       endTime: format(end, 'HH:mm'),
+      // isBooked: 'available',
     };
-    slots.push(slot);
+    const createdSlot = await SlotModel.create(slot);
+    slots.push(createdSlot);
   }
-  await SlotModel.insertMany(slots);
   return slots;
 };
 
+
+const getSlots = async ({ date, service }: Record<string, unknown>) => {
+  if (date && service) {
+    return await SlotModel.find({ date, service }).populate('service');
+  }
+  if (date && !service) {
+    return await SlotModel.find({ date }).populate('service');
+  }
+  if (!date && service) {
+    return await SlotModel.find({ service }).populate('service');
+  }
+  return await SlotModel.find().populate('service');
+}
+
 export const slotService = {
   createSlots,
+  getSlots,
 };

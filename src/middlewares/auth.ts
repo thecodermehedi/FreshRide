ff7fe83cd7e19e4a2b0f2efa739f6dc2902be9c0
Removed Contributor <thecodermehedi@gmail.com>
2024-09-10 12:19:24 +0600

    fix: token extraction problem

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,25 +1,27 @@
 import config from '../config';
 import AppError from '../errors/AppError';
 import UserModel from '../modules/User/user.model';
-import jwt, {type JwtPayload} from 'jsonwebtoken';
-import type {TUserRole} from '../modules/User/user.types';
-import {catchAsync} from '../utils';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import type { TUserRole } from '../modules/User/user.types';
+import { catchAsync } from '../utils';
 import httpStatus from 'http-status';
 
 const auth = (...requiredRoles: Array<TUserRole>) =>
   catchAsync(async (req, res, next) => {
-    let token = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
         'You have no access to this route',
       );
     }
-        if (token.startsWith('Bearer ')) {
-          token = token.slice(7, token.length);
-        }
+    console.log("🚀 ~ file: auth.ts:auth ~ token:", token);
+
     const decoded = jwt.verify(token, config.jwtAccessSecret) as JwtPayload;
-    const user = await UserModel.findOne({email: decoded.email});
+
+    console.log("🚀 ~ file: auth.ts:auth ~ decoded:", decoded);
+
+    const user = await UserModel.findOne({ email: decoded.email });
     if (!user) {
       throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
     }

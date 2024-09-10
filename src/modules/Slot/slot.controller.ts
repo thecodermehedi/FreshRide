f738779d3e5886e3036592fd0579522c77d86f09
Removed Contributor <thecodermehedi@gmail.com>
2024-09-10 18:23:42 +0600

    feat: added getSlots

--- src/modules/Slot/slot.controller.ts
+++ src/modules/Slot/slot.controller.ts
@@ -1,6 +1,6 @@
 import httpStatus from 'http-status';
-import {catchAsync} from '../../utils';
-import {slotService} from './slot.service';
+import { catchAsync } from '../../utils';
+import { slotService } from './slot.service';
 
 const createSlots = catchAsync(async (req, res) => {
   const result = await slotService.createSlots(req.body);
@@ -12,6 +12,27 @@ const createSlots = catchAsync(async (req, res) => {
   });
 });
 
+
+const getSlots = catchAsync(async (req, res) => {
+  const query: Record<string, unknown> = {};
+  const { date, serviceId } = req.query;
+  if (date) {
+    query.date = req.query.date;
+  }
+  if (serviceId) {
+    query.service = req.query.serviceId;
+  }
+  const slots = await slotService.getSlots(query);
+  res.json({
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Slots retrieved successfully',
+    data: slots,
+  });
+});
+
+
 export const slotController = {
   createSlots,
+  getSlots,
 };

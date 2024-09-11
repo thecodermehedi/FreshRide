fc2c38f7fc7a791d98937c653bd59ec6b1072c25
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-11 08:50:01 +0600

    feat: Update booking routes in booking.routes.ts
    - Add booking controller functions to handle POST and GET requests
    - Implement authentication middleware for different user roles

--- src/modules/Booking/booking.routes.ts
+++ src/modules/Booking/booking.routes.ts
@@ -1,13 +1,14 @@
 import auth from '../../middlewares/auth';
 import {createRouter} from '../../utils';
 import {USER_ROLE} from '../User/user.constant';
+import {bookingController} from './booking.controller';
 
 const router = createRouter();
 
-router.post('/', auth(USER_ROLE.USER));
+router.post('/', auth(USER_ROLE.USER), bookingController.bookService);
 
-router.get('/', auth(USER_ROLE.ADMIN));
+router.get('/', auth(USER_ROLE.ADMIN), bookingController.getBookings);
 
-router.get('/my-bookings', auth(USER_ROLE.USER));
+router.get('/my-bookings', auth(USER_ROLE.USER), bookingController.getBookings);
 
 export const bookingRoutes = router;

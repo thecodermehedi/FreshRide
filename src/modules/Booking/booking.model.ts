e76b70cac7ac83e44c63dba81d4e412c322fe016
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-11 08:46:52 +0600

    feat: Add booking model definition

---
+++ src/modules/Booking/booking.model.ts
@@ -0,0 +1,44 @@
+import {model, Schema} from 'mongoose';
+import type {TBooking} from './booking.type';
+
+const bookingSchema = new Schema<TBooking>({
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
+  },
+  service: {
+    type: Schema.Types.ObjectId,
+    ref: 'Service',
+    required: true,
+  },
+  slot: {
+    type: Schema.Types.ObjectId,
+    ref: 'Slot',
+    required: true,
+  },
+  vehicleType: {
+    type: String,
+    enum: [
+      'car',
+      'truck',
+      'SUV',
+      'van',
+      'motorcycle',
+      'bus',
+      'electricVehicle',
+      'hybridVehicle',
+      'bicycle',
+      'tractor',
+    ],
+    required: true,
+  },
+  vehicleBrand: {type: String, required: true},
+  vehicleModel: {type: String, required: true},
+  manufacturingYear: {type: Number, required: true},
+  registrationPlate: {type: String, required: true},
+});
+
+const BookingModel = model<TBooking>('Booking', bookingSchema);
+
+export default BookingModel;

4a4cb6c0ab556d761260fab8381882a844b16f14
Removed Contributor <thecodermehedi@gmail.com>
2024-09-11 18:54:42 +0600

    feat: fixed TBooking type name and added TPayloadUser type

--- src/modules/Booking/booking.type.ts
+++ src/modules/Booking/booking.type.ts
@@ -1,22 +1,27 @@
-import type {ObjectId} from 'mongoose';
+import type { ObjectId } from 'mongoose';
 
 export type TBooking = {
   customer: ObjectId; // Assuming this is an ID reference
-  service: ObjectId; // Assuming this is an ID reference
-  slot: ObjectId; // Assuming this is an ID reference
+  serviceId: ObjectId; // Assuming this is an ID reference
+  slotId: ObjectId; // Assuming this is an ID reference
   vehicleType:
-    | 'car'
-    | 'truck'
-    | 'SUV'
-    | 'van'
-    | 'motorcycle'
-    | 'bus'
-    | 'electricVehicle'
-    | 'hybridVehicle'
-    | 'bicycle'
-    | 'tractor';
+  | 'car'
+  | 'truck'
+  | 'SUV'
+  | 'van'
+  | 'motorcycle'
+  | 'bus'
+  | 'electricVehicle'
+  | 'hybridVehicle'
+  | 'bicycle'
+  | 'tractor';
   vehicleBrand: string;
   vehicleModel: string;
   manufacturingYear: number;
   registrationPlate: string;
 };
+
+export type TPayloadUser = {
+  email: string;
+  role: string;
+}

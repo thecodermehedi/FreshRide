ff44218f458d7b6c5167a6a85ef7fa1c3ba816a8
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-11 08:47:08 +0600

    feat: Add booking controller with bookService and getBookings functions

---
+++ src/modules/Booking/booking.controller.ts
@@ -0,0 +1,32 @@
+import type {RequestHandler} from 'express';
+import {catchAsync} from '../../utils';
+import httpStatus from 'http-status';
+import {bookingService} from './booking.service';
+
+const bookService: RequestHandler = catchAsync(async (req, res) => {
+  const booking = await bookingService.bookService(req.body, req.user);
+  res.json({
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Booking successfully',
+    data: booking,
+  });
+});
+
+const getBookings: RequestHandler = catchAsync(async (req, res) => {
+  const bookings = await bookingService.getBookings(req.user);
+  res.json({
+    success: true,
+    statusCode: httpStatus.OK,
+    message:
+      req.user.role === 'admin'
+        ? 'All bookings retrieved successfully'
+        : 'User bookings retrieved successfully',
+    data: bookings,
+  });
+});
+
+export const bookingController = {
+  bookService,
+  getBookings,
+};

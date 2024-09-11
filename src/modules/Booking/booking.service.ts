b276dca91e19603d8dfd1c70a7870116e86e46ab
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-11 08:47:15 +0600

    feat: Add booking controller service functions
    Add bookingService functions for creating and retrieving bookings based on the user's role.

---
+++ src/modules/Booking/booking.service.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const bookService = async (bookingData, bookingUser) => {
+  //todo: based on booing user role i.e. admin or user => create booking
+};
+
+const getBookings = async (bookingUser) => {
+  //todo: based on booing user role i.e. admin or user => get bookings
+};
+
+export const bookingService = {
+  bookService,
+  getBookings,
+};

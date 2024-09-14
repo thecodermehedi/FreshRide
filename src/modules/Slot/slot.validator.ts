c60e1c310299eea4fd985656b46e7718efb9055a
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-14 09:09:16 +0600

    Refactor slot.validator.ts to improve validation schema for creating slots

--- src/modules/Slot/slot.validator.ts
+++ src/modules/Slot/slot.validator.ts
@@ -1,20 +1,30 @@
 import {z} from 'zod';
 
 const createSlotValidationSchema = z.object({
-  body: z
-    .object({
-      service: z.string(),
-      startDate: z.coerce.date(),
-      endDate: z.coerce.date(),
-      isBooked: z
-        .enum(['available', 'cancelled', 'booked'])
-        .default('available')
-        .optional(),
-    })
-    .refine((data) => data.startDate < data.endDate, {
-      message: 'End date must be after start date',
-      path: ['endDate'],
+  body: z.object({
+    service: z
+      .string({
+        invalid_type_error: 'Service must be a string',
+      })
+      .min(1, {message: 'Service is required'}),
+    date: z.string({
+      invalid_type_error: 'Date must be a string',
     }),
+    startTime: z
+      .string({
+        invalid_type_error: 'Start Time must be a string',
+      })
+      .min(1, {message: 'Start time is required'}),
+    endTime: z
+      .string({
+        invalid_type_error: 'End Time must be a string',
+      })
+      .min(1, {message: 'End time is required'}),
+    isBooked: z
+      .enum(['available', 'cancelled', 'booked'])
+      .default('available')
+      .optional(),
+  }),
 });
 
 export const slotValidations = {

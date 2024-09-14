fed4db9dedff866c2d746575f8a12e1ed7379745
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-14 09:09:23 +0600

    Refactor route configuration to add endpoint for retrieving user bookings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express';
-import { authRoutes } from '../modules/Auth/auth.route';
-import { serviceRoutes } from '../modules/Service/service.routes';
-import { slotRoutes } from '../modules/Slot/slot.routes';
-import { bookingRoutes } from '../modules/Booking/booking.routes';
+import {Router} from 'express';
+import {authRoutes} from '../modules/Auth/auth.route';
+import {serviceRoutes} from '../modules/Service/service.routes';
+import {slotRoutes} from '../modules/Slot/slot.routes';
+import {bookingRoutes} from '../modules/Booking/booking.routes';
+import auth from '../middlewares/auth';
+import {USER_ROLE} from '../modules/User/user.constant';
+import {bookingController} from '../modules/Booking/booking.controller';
 
 const router = Router();
 
@@ -22,7 +25,11 @@ const moduleRoutes = [
   {
     path: '/bookings',
     entry: bookingRoutes,
-  }
+  },
+  {
+    path: '/my-bookings',
+    entry: router.get('/', auth(USER_ROLE.USER), bookingController.getBookings),
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.entry));

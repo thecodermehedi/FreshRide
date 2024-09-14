8e29d45af00edfdb223ed9d4a3444b9f15417ad5
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-14 09:09:04 +0600

    Refactor slot.types.ts to change the type of 'isBooked' from 'cacelled' to 'cancelled' and 'Date' to 'string'

--- src/modules/Slot/slot.types.ts
+++ src/modules/Slot/slot.types.ts
@@ -1,11 +1,11 @@
 import type {ObjectId} from 'mongoose';
 
-export type TIsBooked = 'available' | 'cacelled' | 'booked';
+export type TIsBooked = 'available' | 'cancelled' | 'booked';
 
 export type TSlot = {
   service: ObjectId;
-  date: Date;
+  date: string;
   startTime: string;
   endTime: string;
-  isBooked: TIsBooked;
+  isBooked?: TIsBooked;
 };

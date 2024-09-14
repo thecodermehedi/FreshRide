608d93a694f5473e6c1ee9976a8584b71020f3f3
Removed Contributor <iammehedihasan@protonmail.com>
2024-09-14 09:08:22 +0600

    Refactor booking.routes.ts for improved validation and security

--- src/modules/Booking/booking.routes.ts
+++ src/modules/Booking/booking.routes.ts
@@ -1,14 +1,15 @@
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import {createRouter} from '../../utils';
 import {USER_ROLE} from '../User/user.constant';
 import {bookingController} from './booking.controller';
+import { bookingValidations } from './booking.validator';
 
 const router = createRouter();
 
-router.post('/', auth(USER_ROLE.USER), bookingController.bookService);
+router.post('/', auth(USER_ROLE.USER), validateRequest(bookingValidations.createBookingValidationSchema), bookingController.bookService);
 
 router.get('/', auth(USER_ROLE.ADMIN), bookingController.getBookings);
 
-router.get('/my-bookings', auth(USER_ROLE.USER), bookingController.getBookings);
 
 export const bookingRoutes = router;

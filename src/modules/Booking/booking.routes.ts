b1556c035225f319399fe0ec5d65cfd2fc0a176f
Removed Contributor <thecodermehedi@gmail.com>
2024-09-17 18:57:38 +0600

    reformatted

--- src/modules/Booking/booking.routes.ts
+++ src/modules/Booking/booking.routes.ts
@@ -1,19 +1,14 @@
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
-import {createRouter} from '../../utils';
-import {USER_ROLE} from '../User/user.constant';
-import {bookingController} from './booking.controller';
-import {bookingValidations} from './booking.validator';
+import { createRouter } from '../../utils';
+import { USER_ROLE } from '../User/user.constant';
+import { bookingController } from './booking.controller';
+import { bookingValidations } from './booking.validator';
 
 const router = createRouter();
 
-router.post(
-  '/',
-  auth(USER_ROLE.USER),
-  validateRequest(bookingValidations.createBookingValidationSchema),
-  bookingController.bookService,
-);
-
 router.get('/', auth(USER_ROLE.ADMIN), bookingController.getBookings);
+
+router.post('/', auth(USER_ROLE.USER), validateRequest(bookingValidations.createBookingValidationSchema), bookingController.bookService);
 
 export const bookingRoutes = router;

0838e746b26e834cd80f91988600252e881ab862
Removed Contributor <thecodermehedi@gmail.com>
2024-09-17 18:56:02 +0600

    uncommented error details

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -6,16 +6,16 @@ import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
-// import type {TErrorObject} from '../errors/error.types';
+import type {TErrorObject} from '../errors/error.types';
 import type {ErrorRequestHandler} from 'express';
-// import {getCurrentDateTime} from '../utils';
+import {getCurrentDateTime} from '../utils';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let errorCode: number = httpStatus.INTERNAL_SERVER_ERROR;
   let errorMessage: string = 'Something went wrong';
-  // let errorDetails: Array<TErrorObject> = [
-    // {path: '', message: 'Something went wrong'},
-  // ];
+  let errorDetails: Array<TErrorObject> = [
+  {path: '', message: 'Something went wrong'},
+  ];
 
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
@@ -24,7 +24,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
       config.nodeEnv !== 'production'
         ? simplifiedError.message
         : 'Validation Error';
-    // errorDetails = simplifiedError.details;
+    errorDetails = simplifiedError.details;
   }
 
   if (err.name === 'ValidationError') {
@@ -32,7 +32,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     errorCode = shortError.code;
     errorMessage =
       config.nodeEnv === 'production' ? shortError.message : err.message;
-    // errorDetails = shortError.details;
+    errorDetails = shortError.details;
   }
 
   if (err.name === 'CastError') {
@@ -42,7 +42,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
       config.nodeEnv !== 'production'
         ? shortError.message
         : `Invalid ${err.kind} Error`;
-    // errorDetails = shortError.details;
+    errorDetails = shortError.details;
   }
 
   if (err.code === 11000) {
@@ -52,35 +52,35 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
       config.nodeEnv !== 'production'
         ? shortError.message
         : 'Duplicate Key Error';
-    // errorDetails = shortError.details;
+    errorDetails = shortError.details;
   }
 
   if (err instanceof AppError) {
     errorCode = err.code;
     errorMessage = err.message;
-    // errorDetails = [{path: err.path, message: err.message}];
+    errorDetails = [{path: err.path, message: err.message}];
   }
 
   if (err instanceof Error) {
     errorMessage = err.message;
-    // errorDetails = [{path: '', message: err.message}];
+    errorDetails = [{path: '', message: err.message}];
   }
 
   res.status(errorCode).json({
     success: false,
     statusCode: errorCode,
     message: errorMessage,
-    // ...(config.nodeEnv !== 'production' ? {details: errorDetails} : {}),
-    // timestamp: getCurrentDateTime(),
-    // ...(config.nodeEnv !== 'production'
-    //   ? {
-    //       debugInfo: {
-    //         method: req.method ?? 'no method provided',
-    //         url: req.url ?? 'no url provided',
-    //         stack: err.stack ?? 'no stack provided',
-    //       },
-    //     }
-    //   : {}),
+    ...(config.nodeEnv !== 'production' ? {details: errorDetails} : {}),
+    timestamp: getCurrentDateTime(),
+    ...(config.nodeEnv !== 'production'
+      ? {
+          debugInfo: {
+            method: req.method ?? 'no method provided',
+            url: req.url ?? 'no url provided',
+            stack: err.stack ?? 'no stack provided',
+          },
+        }
+      : {}),
   });
 
   next(err);

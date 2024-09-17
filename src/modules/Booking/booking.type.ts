007cde43defd5968cb46ded550225b29869cb3c8
Removed Contributor <thecodermehedi@gmail.com>
2024-09-17 18:58:30 +0600

    refactored TBooking and created an extra TVehicle

--- src/modules/Booking/booking.type.ts
+++ src/modules/Booking/booking.type.ts
@@ -1,27 +1,24 @@
 import type {ObjectId} from 'mongoose';
 
+export type TVehicle =
+  | 'car'
+  | 'truck'
+  | 'SUV'
+  | 'van'
+  | 'motorcycle'
+  | 'bus'
+  | 'electricVehicle'
+  | 'hybridVehicle'
+  | 'bicycle'
+  | 'tractor';
+
 export type TBooking = {
-  customer: ObjectId; // Assuming this is an ID reference
-  serviceId: ObjectId; // Assuming this is an ID reference
-  slotId: ObjectId; // Assuming this is an ID reference
-  vehicleType:
-    | 'car'
-    | 'truck'
-    | 'SUV'
-    | 'van'
-    | 'motorcycle'
-    | 'bus'
-    | 'electricVehicle'
-    | 'hybridVehicle'
-    | 'bicycle'
-    | 'tractor';
+  customer?: ObjectId;
+  serviceId: ObjectId;
+  slotId: ObjectId;
+  vehicleType: TVehicle;
   vehicleBrand: string;
   vehicleModel: string;
   manufacturingYear: number;
   registrationPlate: string;
-};
-
-export type TPayloadUser = {
-  email: string;
-  role: string;
 };

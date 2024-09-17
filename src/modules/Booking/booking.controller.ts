122335f2b2428fe32c19cfba6dbeffa7495d8766
Removed Contributor <thecodermehedi@gmail.com>
2024-09-17 18:57:21 +0600

    fixed code format and bookservice props

--- src/modules/Booking/booking.controller.ts
+++ src/modules/Booking/booking.controller.ts
@@ -1,23 +1,28 @@
-import type {RequestHandler} from 'express';
-import {catchAsync, sendResponse} from '../../utils';
+import type { RequestHandler } from 'express';
+import { catchAsync, sendResponse } from '../../utils';
 import httpStatus from 'http-status';
-import {bookingService} from './booking.service';
-import type {TPayloadUser} from './booking.type';
+import { bookingService } from './booking.service';
 
 const bookService: RequestHandler = catchAsync(async (req, res) => {
-  const booking = await bookingService.bookService(
-    req.body,
-    req.user as TPayloadUser,
-  );
-  sendResponse(res, httpStatus.OK, 'Booking successfully', booking);
+  const booking = await bookingService.bookService(req.body, req.user);
+  sendResponse(res, httpStatus.OK, "Booking successful", booking);
 });
 
 const getBookings: RequestHandler = catchAsync(async (req, res) => {
-  const bookings = await bookingService.getBookings(req.user as TPayloadUser);
+  const bookings = await bookingService.getBookings(req.user);
   if (bookings.length === 0) {
-    return sendResponse(res, httpStatus.NOT_FOUND, 'No Data found', bookings, false);
+    return sendResponse(
+      res,
+      httpStatus.NOT_FOUND,
+      'No Data found',
+      bookings,
+      false,
+    );
   }
-  const message = req.user.role === 'admin' ? 'All bookings retrieved successfully' : 'User bookings retrieved successfully';
+  const message =
+    req.user.role === 'admin'
+      ? 'All bookings retrieved successfully'
+      : 'User bookings retrieved successfully';
   return sendResponse(res, httpStatus.OK, message, bookings);
 });
 
